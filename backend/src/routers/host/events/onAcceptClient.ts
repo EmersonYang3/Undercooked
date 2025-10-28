5fef4ed4a7a09dea3a16d92fd7cd652b0028e06e
EmersonYang3 <emersonyang28@gmail.com>
2025-10-28 10:30:31 -0400

    Log client lobby status on accept event
    Added logic to check if the accepted client is present in the current lobby and log the result. This provides more context during the client acceptance process for debugging and monitoring.

--- backend/src/routers/host/events/onAcceptClient.ts
+++ backend/src/routers/host/events/onAcceptClient.ts
@@ -2,12 +2,18 @@ import type { uniqueIdentifier } from "shared/types";
 import type { Socket } from "socket.io";
 
 import socketRegistry from "services/socketRegistry";
+import lobbyService from "services/lobby"
 
 function onAcceptClient(socket: Socket, identifier: uniqueIdentifier) {
     const clientConnection = socketRegistry.getSocketConnectionById(identifier);
+    const currentLobbyData = lobbyService.getLobbyData()
+
+    const clients = currentLobbyData.clients || [];
+    const isClientInLobby = clients.find(client => client.identifier === identifier);
 
     console.log('Host accepted client with identifier:', identifier);
     console.log('Client connection details:', clientConnection.socket.data);
-}
+    console.log('Client is in lobby:', isClientInLobby !== undefined);
+}   
 
 export default onAcceptClient
