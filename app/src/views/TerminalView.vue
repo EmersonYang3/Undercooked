4a2ee6cb34203fe71325b5530552d3cb5f88ac79
johnnyw <jwu38438@gmail.com>
2025-11-28 14:05:58 -0500

    making the join part reusable

--- app/src/views/TerminalView.vue
+++ app/src/views/TerminalView.vue
@@ -1,15 +1,10 @@
 <template>
-    <div>
-
-    </div>
+    <JoinGame role="station"></JoinGame>
 </template>
 
 <script setup lang="ts">
-
-
-
-
-
+import JoinGame from '@/components/JoinGame.vue';
+//route based off of the station type that got set
 </script>
 
 <style scoped>
