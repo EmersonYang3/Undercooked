a82455a1337e9c0d6df0c33ba4b910129cc0df58
Lucille <144396231+EmersonYang3@users.noreply.github.com>
2025-12-10 11:13:39 -0500

    Remove instantiateLobbyData and add itemPlacer logic
    Deleted unused instantiateLobbyData.ts. Added canPlaceFoodInStation function to itemPlacer.ts to validate food placement in stations based on shared data and lobby state.

--- backend/src/utils/Singletons/itemPlacer.ts
+++ backend/src/utils/Singletons/itemPlacer.ts
@@ -1,1 +1,25 @@
+import { uniqueIdentifier, internalStationData, internalFoodData } from "shared/types"
+
 import lobbyService from "services/lobby"
+import sharedData from "shared/data"
+
+const foodData = sharedData.foodData
+const stationData = sharedData.stationData
+
+function canPlaceFoodInStation(food: string, stationIdentifier: uniqueIdentifier): boolean {
+    const lobbyData = lobbyService.getLobbyData()
+    const targetStationData = lobbyData.stationData[stationIdentifier]
+    if (!targetStationData) { return false }
+
+    const stationType: string = targetStationData.stationType
+    const internalStationData: internalStationData = stationData[stationType]
+    const internalFoodData: internalFoodData = foodData[food]
+    if (!internalFoodData || !internalStationData) { return false }
+
+    const stationMethod = internalStationData.method
+    const isMethodValid = internalFoodData.methods[stationMethod] !== undefined
+
+    return isMethodValid
+}
+
+export default { canPlaceFoodInStation }
