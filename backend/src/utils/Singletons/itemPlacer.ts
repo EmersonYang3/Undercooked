e21168afbafe7521e3e2666d12f7b6b09e26d3e5
Lucille <144396231+EmersonYang3@users.noreply.github.com>
2025-12-11 11:08:07 -0500

    Add player item management functions and enum
    Introduces removePlayerItem and givePlayerItem functions to manage player-held items in the lobby. Also adds setCurrentItem to sharedRemotes enum for socket communication.

--- backend/src/utils/Singletons/itemPlacer.ts
+++ backend/src/utils/Singletons/itemPlacer.ts
@@ -1,10 +1,15 @@
-import { uniqueIdentifier, internalStationData, internalFoodData } from "shared/types"
+import { uniqueIdentifier, internalStationData, internalFoodData, foodItem, plate } from "shared/types"
 
 import lobbyService from "services/lobby"
+import socketRegistry from "services/socketRegistry"
+
 import sharedData from "shared/data"
+import sharedEnums from "shared/enums"
 
 const foodData = sharedData.foodData
 const stationData = sharedData.stationData
+
+const ServerSharedRemotes = sharedEnums.sharedRemotes
 
 function canPlaceFoodInStation(food: string, stationIdentifier: uniqueIdentifier): boolean {
     const lobbyData = lobbyService.getLobbyData()
@@ -22,4 +27,43 @@ function canPlaceFoodInStation(food: string, stationIdentifier: uniqueIdentifier
     return isMethodValid
 }
 
-export default { canPlaceFoodInStation }
+function removePlayerItem(identifier: uniqueIdentifier): foodItem | plate | undefined {
+    let playerHeldItem: foodItem | plate | undefined = undefined
+
+    lobbyService.transformLobbyData((lobbyData) => {
+        const playerData = lobbyData.playerData[identifier]
+        const playerSocketConnection = socketRegistry.getSocketConnectionById(identifier)
+        if (!playerData || !playerSocketConnection) { return lobbyData }
+
+        playerHeldItem = playerData.currentlyHeldItem
+        if (!playerHeldItem) { return lobbyData }
+
+        playerData.currentlyHeldItem = undefined
+        playerData.isHoldingPlate = false
+
+        lobbyData.playerData[identifier] = playerData
+        playerSocketConnection.socket.emit(ServerSharedRemotes.setCurrentItem, null, false)
+
+        return lobbyData
+    })
+
+    return playerHeldItem
+}
+
+function givePlayerItem(identifier: uniqueIdentifier, item: foodItem | plate, isRecievingPlate: boolean) {
+    lobbyService.transformLobbyData((lobbyData) => {
+        const playerData = lobbyData.playerData[identifier]
+        const playerSocketConnection = socketRegistry.getSocketConnectionById(identifier)
+        if (!playerData || playerData.currentlyHeldItem || !playerSocketConnection) { return lobbyData }
+
+        playerData.currentlyHeldItem = item
+        playerData.isHoldingPlate = isRecievingPlate
+
+        lobbyData.playerData[identifier] = playerData
+        playerSocketConnection.socket.emit(ServerSharedRemotes.setCurrentItem, item, isRecievingPlate)
+
+        return lobbyData
+    })
+}
+
+export default { canPlaceFoodInStation, removePlayerItem, givePlayerItem }
