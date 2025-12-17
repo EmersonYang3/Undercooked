6e4c9d446465c4f1cc43c4f72697968075543547
Lucille <144396231+EmersonYang3@users.noreply.github.com>
2025-12-17 10:57:52 -0500

    Add handler for hostRejectedConnection event
    Registered the onRejectConnection handler for the hostRejectedConnection event in the host events router. This allows the host to handle rejected connection events from shared remotes.

--- backend/src/routers/host/events.ts
+++ backend/src/routers/host/events.ts
@@ -2,12 +2,15 @@ import sharedEnums from "shared/enums";
 
 import onClientAccept from "./events/onAcceptClient"
 import onAcceptStation from "./events/onAcceptStation"
-import attemptGameStart from "./events/attemptGameStart";
+import attemptGameStart from "./events/attemptGameStart"
+import onRejectConnection from "./events/onRejectConnection"
 
 const hostToServerRemotes = sharedEnums.hostToServerRemotes
+const hostToSharedRemotes = sharedEnums.sharedRemotes
 
 export default {
     [hostToServerRemotes.acceptClientJoin]: onClientAccept,
     [hostToServerRemotes.acceptStationJoin]: onAcceptStation,
-    [hostToServerRemotes.startLobby]: attemptGameStart
+    [hostToServerRemotes.startLobby]: attemptGameStart,
+    [hostToSharedRemotes.hostRejectedConnection]: onRejectConnection
 }
