f43cce79356a17773c02588c9dd1b159dc73b295
Lucille <144396231+EmersonYang3@users.noreply.github.com>
2026-03-04 11:02:40 -0500

    Remove Testing Code

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,14 +5,4 @@
 
 <script setup lang="ts">
 import NotificationsView from './views/Global/NotificationsView.vue';
-
-import KeyBindService from './services/KeyBindService';
-
-KeyBindService.OnKeyHeldDown(" ", () => {
-  console.log("space is being held down")
-})
-
-KeyBindService.OnKeyReleased(" ", (duration: number) => {
-  console.log(`space was released after being held for ${duration}ms`)
-})
 </script>
