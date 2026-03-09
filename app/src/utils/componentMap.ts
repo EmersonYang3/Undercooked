93400653a53ba98b77ada6ce348b6aba87563abf
egggggggggggggggggggggg <144480382+egggggggggggggggggggggg@users.noreply.github.com>
2026-03-09 11:33:13 -0400

    Migrate stuff and clean up
    Rewrote TerminalRoom to utilie KeyBindService over the previous method of listening. Removed waiting logic which was seperated to another component prior. Removal of several variables that didn't serve a purpose. Resolved some component path issues in componentMap.ts

--- app/src/utils/componentMap.ts
+++ app/src/utils/componentMap.ts
@@ -1,23 +1,34 @@
 
 import { Component } from "vue";
 import sharedEnums from "@shared/enums";
-import Submit from "@/components/ActualStations/Static/Submit.vue";
-import Dispense from "@/components/ActualStations/Static/Dispense.vue";
-import Dispose from "@/components/ActualStations/Static/Dispose.vue";
-import Empty from "@/components/ActualStations/Static/Empty.vue";
-import Blending from "@/components/ActualStations/Gameplay/Blending.vue";
-import Dicing from "@/components/ActualStations/Gameplay/Dicing.vue";
-import Boiling from "@/components/ActualStations/Gameplay/Boiling.vue";
-import Frying from "@/ActualStations/Gameplay/Frying.vue";
+import Submit from "@/components/Stations/Static/Submit.vue";
+import Dispense from "@/components/Stations/Static/Dispense.vue";
+import Dispose from "@/components/Stations/Static/Dispose.vue";
+import Empty from "@/components/Stations/Static/Empty.vue";
+import Blending from "@/components/Stations/Gameplay/Blending.vue";
+import Dicing from "@/components/Stations/Gameplay/Dicing.vue";
+import Boiling from "@/components/Stations/Gameplay/Boiling.vue";
+import Frying from "@/components/Stations/Gameplay/Frying.vue";
 
 let stationTypes = sharedEnums.stationTypes;
 
+
+
+/* Static components 
+- Defined as components that don't implement any sort of gameplay and 
+only really do animations or show stuff.
+*/
 export const staticComponentsMap: Record<string, Component> = {
     [stationTypes.submitStation]: Submit,
     [stationTypes.dispenseStation]: Dispense,
     [stationTypes.disposeStation]: Dispose,
     [stationTypes.emptyStation]: Empty,
 }
+
+/* Dynamic components 
+- Defined as components that do implement some sort of gameplay and requires transfering 
+total screen control to the component for gameplay. 
+*/
 export const dynamicComponentsMap: Record<string, Component> = {
     [stationTypes.blendingStation]: Blending,
     [stationTypes.dicingStation]: Dicing,
--- app/src/views/Game/TerminalRoom.vue
+++ app/src/views/Game/TerminalRoom.vue
@@ -1,12 +1,70 @@
 <template>
-
+    <div>
+        <component 
+            v-if="visible" 
+            :is="selectedComponent"  
+            :items="items"  
+            @completed="completedGame"
+        ></component>
+    </div>
 </template>
 
 <script setup lang="ts">
-import { useStationStore } from '@/stores/Roles/StationStore'
+import KeyBindService from '@/services/KeyBindService';
+import { useStationStore } from '@/stores/Roles/StationStore';
+import { useSocketStore } from '@/stores/SocketStore';
+import { dynamicComponentsMap, staticComponentsMap } from '@/utils/componentMap';
+import sharedEnums from '@shared/enums';
+import { holdableItem } from '@shared/types';
+import { Component, onMounted, onUnmounted, ref } from 'vue';
 
-const stationStore = useStationStore()
-const stationType = stationStore.getStationRole()
 
-console.warn(stationType)
+const stationStore = useStationStore();
+const stationRole = stationStore.getStationRole();
+const socketStore = useSocketStore();
+const items = ref<holdableItem | null>(null);
+
+const staticComp = ref<boolean>(false);
+// Controls visibility for the component if its dynamic
+const visible = ref<boolean>(false);
+const selectedComponent = ref<Component | null>(null);
+
+
+socketStore.attachEventListener(sharedEnums.sharedRemotes.setCurrentItem, 
+    (item: holdableItem) => {
+        if (!item || typeof item !== "object") {
+            console.error("Invalid item:", item);
+            return;
+        }
+        items.value = item;
+    }
+);
+/* Only fires on dynamicComponents */
+function completedGame() {
+    visible.value = false;
+    KeyBindService.SetSpecialKeyListenFlag(true);
+    
+}
+function triggerGamplay() {
+    //Haven't tested this so it might have unexpected behavior.
+    KeyBindService.SetSpecialKeyListenFlag(false);
+    //Don't do anything as the component is already visible 
+    if (staticComp.value) return;
+}
+
+onMounted(()=> {    
+    let keys = Object.keys(staticComponentsMap);
+    if (keys.includes(stationRole)) {
+        staticComp.value = true;
+        visible.value = true;
+        selectedComponent.value = staticComponentsMap[stationRole] 
+    } else {
+        selectedComponent.value = dynamicComponentsMap[stationRole]
+    }
+    KeyBindService.SetSpecialKeyListenFlag(true);
+    KeyBindService.OnKeyHeldDown(" ", triggerGamplay);
+})
+onUnmounted(() => {
+    socketStore.removeAllEventListeners();
+})
 </script>
